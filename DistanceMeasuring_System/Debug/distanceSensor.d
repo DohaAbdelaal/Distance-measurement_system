2ecb7102715af0e4bdddf690d6bf747306e98cb2
DohaAbdelaal <doha.abdelaal@gmail.com>
2023-07-25 15:59:47 +0300

    initial commit

---
+++ DistanceMeasuring_System/Debug/distanceSensor.d
@@ -0,0 +1,10 @@
+distanceSensor.o distanceSensor.o: ../distanceSensor.c \
+  ../distanceSensor.h ../std_types.h ../icu.h ../GPIO.h
+
+../distanceSensor.h:
+
+../std_types.h:
+
+../icu.h:
+
+../GPIO.h:
